07e6004570bafba5d4f350307acacae8071db619
wulan marsuri <marsuriwulan@gmail.com>
2022-06-26 09:50:48 +0700

    make table warga and keuagan

---
+++ src/migrations/20220624093654-create-keuangan.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('keuangans', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id_keuangan: {
+        type: Sequelize.INTEGER
+      },
+      nominal: {
+        type: Sequelize.INTEGER
+      },
+      warga_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('keuangans');
+  }
+};
---
+++ src/models/keuangan.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class keuangan extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      models.keuangan.belongsTo(models.warga, {
+        foreignKey: 'warga_id'
+      })
+    }
+  }
+  keuangan.init({
+    id_keuangan: DataTypes.INTEGER,
+    nominal: DataTypes.INTEGER,
+    warga_id: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'keuangan',
+  });
+  return keuangan;
+};
